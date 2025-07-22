54ea19f35dbf1397e5235bc7f75a7b7e7e91aa3f
Albinksss <albin.gef@gmail.com>
2025-07-22 15:47:29 +0200

    Add files via upload

--- app_frontend.py
+++ app_frontend.py
@@ -7,7 +7,7 @@ import matplotlib.cm as cm
 
 # Configuration API
 #API_BASE = "http://127.0.0.1:8000/"
-API_BASE = "https://backend-xowl.onrender.com"
+API_BASE = "https://backend-xowl.onrender.com/"
 UPLOAD_ENDPOINT = f"{API_BASE}upload/"
 PROCESS_ENDPOINT = f"{API_BASE}process/"
 
